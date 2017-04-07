ac8e9f1dc78cdd778dd4c06bb6c32ed6cb2bfed1
Mazy <mazongyong@163.com>
2017-04-07 13:14:41 +0800

    重构项目结构

---
+++ MiaoShow/MiaoShow/Classes/Main/XMNavigationController.swift
@@ -0,0 +1,35 @@
+//
+//  XMNavigationController.swift
+//  MiaoShow
+//
+//  Created by  Mazy on 2017/4/7.
+//  Copyright © 2017年 Mazy. All rights reserved.
+//
+
+import UIKit
+
+class XMNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
