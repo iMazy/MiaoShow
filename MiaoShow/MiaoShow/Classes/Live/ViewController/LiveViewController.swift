14a4d13b8df9f66332c473facc7a38f4230e6407
Mazy <mazongyong@163.com>
2017-04-08 09:24:15 +0800

    设置向上滚动时隐藏导航栏和标签栏

--- MiaoShow/MiaoShow/Classes/Live/ViewController/LiveViewController.swift
+++ MiaoShow/MiaoShow/Classes/Live/ViewController/LiveViewController.swift
@@ -8,28 +8,89 @@
 
 import UIKit
 
+let cellID = "MAZY"
+let screenH = UIScreen.main.bounds.height
+
 class LiveViewController: XMBaseViewController {
+    
+    /// 懒加载tableView
+    lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: UIScreen.main.bounds, style: .plain)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.contentInset = UIEdgeInsetsMake(64, 0, 49, 0)
+        tableView.scrollIndicatorInsets = tableView.contentInset
+        return tableView
+    }()
+    
+    var currentOffsetY: CGFloat?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        // 取消顶部自动约束
+        automaticallyAdjustsScrollViewInsets = false
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
+        
+        view.addSubview(tableView)
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+
+extension LiveViewController:UITableViewDataSource,UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID)
+        cell?.textLabel?.text = "ddafagaga"
+        return cell!
+    }
+}
+
+
+// MARK: - scrollViewDelegate
+extension LiveViewController {
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        currentOffsetY = scrollView.contentOffset.y
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let rect = tabBarController?.tabBar.frame
+        
+        if scrollView.contentOffset.y > currentOffsetY ?? 0 {
+            let distance = scrollView.contentOffset.y - currentOffsetY!
+            UIView.animate(withDuration: 0.5, animations: { 
+                if distance < 49.0 {
+                    self.tabBarController?.tabBar.frame = CGRect(x: (rect?.origin.x)!, y: screenH+distance, width: (rect?.width)!, height: (rect?.height)!)
+                }
+                self.navigationController?.setNavigationBarHidden(true, animated: true)
+            })
+        } else {
+            UIView.animate(withDuration: 0.5, animations: { 
+                self.navigationController?.setNavigationBarHidden(false, animated: true)
+                self.tabBarController?.tabBar.frame = CGRect(x: (rect?.origin.x)!, y: screenH-49, width: (rect?.width)!, height: (rect?.height)!)
+            })
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
