fe04c70a68c82ec6a1c1925f5cdf4e027c5ee73f
Mazy <mazongyong@163.com>
2017-04-08 10:06:59 +0800

    自定义主页cell

---
+++ MiaoShow/MiaoShow/Classes/Live/ViewController/LiveTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  LiveTableViewCell.swift
+//  MiaoShow
+//
+//  Created by  Mazy on 2017/4/8.
+//  Copyright © 2017年 Mazy. All rights reserved.
+//
+
+import UIKit
+
+class LiveTableViewCell: UITableViewCell {
+
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
