eb22ac2356b66e9bf58d42a0b64cbc2c95b045a1
Mazy <mazongyong@163.com>
2017-04-18 22:05:51 +0800

    动画展示直播bar

--- MiaoShow/MiaoShow/Classes/Live/ViewController/LiveRecordController.swift
+++ MiaoShow/MiaoShow/Classes/Live/ViewController/LiveRecordController.swift
@@ -10,13 +10,49 @@ import UIKit
 
 class LiveRecordController: UIViewController {
 
+    
+    @IBOutlet weak var bottomContentView: UIImageView!
+    @IBOutlet weak var bottomContraint: NSLayoutConstraint!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.black.withAlphaComponent(0.3)
+        
+        bottomContentView.layoutIfNeeded()
+        bottomContentView.setNeedsLayout()
 
-        view.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+
+        bottomContentView.transform = CGAffineTransform(translationX: 0, y: bottomContentView.bounds.height)
+        
+        
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        UIView.animate(withDuration: 0.3, delay: 0, options: [UIViewAnimationOptions.curveLinear], animations: {
+            self.bottomContentView.transform = .identity
+        }, completion: nil)
+    }
+
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         dismiss(animated: false, completion: nil)
     }
+    
+    
+    @IBAction func beginRecord() {
+        print("录制直播")
+    }
+    
+    @IBAction func shortVideoAction() {
+        print("短视频")
+    }
+    
+    @IBAction func uploadAction() {
+        print("上传")
+    }
+    
 }
