d584ddb022a4b5b5d30340d7e9f4a8ec26e38018
Mazy <mazongyong@163.com>
2017-04-26 17:48:53 +0800

    feat: 完成我的界面展示

--- MiaoShow/MiaoShow/Classes/Me/ViewController/MeViewController.swift
+++ MiaoShow/MiaoShow/Classes/Me/ViewController/MeViewController.swift
@@ -23,6 +23,7 @@ class MeViewController: XMBaseViewController {
         // Do any additional setup after loading the view.
         
         tableView.dataSource = self
+        tableView.delegate = self
         
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         
@@ -31,11 +32,26 @@ class MeViewController: XMBaseViewController {
         titleArray = [["我的喵币","直播间管理","我的短视频"],["我的收益","游戏中心"],["设置"]]
         imageArray = [["my_coin","live_manager","shortVideo"],["income","game_center"],["setting"]]
         
+        let headerView = Bundle.main.loadNibNamed("MeHeaderViwe", owner: nil, options: [:])?.last as? MeHeaderViwe
+        
+        
+        headerView?.frame = CGRect(origin: CGPoint.zero, size: CGSize(width: view.bounds.width, height: 200))
+        tableView.tableHeaderView = headerView
+        
+        tableView.contentInset = UIEdgeInsetsMake(-20, 0, 0, 0)
+        
+        tableView.sectionFooterHeight = 0
+        
         
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+    
 }
 
-extension MeViewController: UITableViewDataSource {
+extension MeViewController: UITableViewDataSource,UITableViewDelegate {
     
     func numberOfSections(in tableView: UITableView) -> Int {
         return titleArray.count
@@ -51,5 +67,9 @@ extension MeViewController: UITableViewDataSource {
         cell?.imageView?.image = UIImage(named: imageArray[indexPath.section][indexPath.row])
         cell?.textLabel?.text = titleArray[indexPath.section][indexPath.row]
         return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
