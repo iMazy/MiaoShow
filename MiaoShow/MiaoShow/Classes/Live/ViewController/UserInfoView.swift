f2776d184d8727a19b0594e3acf7b676a7e1bd86
Mazy <mazongyong@163.com>
2017-04-29 22:33:26 +0800

    添加用户视图

---
+++ MiaoShow/MiaoShow/Classes/Live/ViewController/UserInfoView.swift
@@ -0,0 +1,21 @@
+//
+//  UserInfoView.swift
+//  MiaoShow
+//
+//  Created by  Mazy on 2017/4/29.
+//  Copyright © 2017年 Mazy. All rights reserved.
+//
+
+import UIKit
+
+class UserInfoView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
